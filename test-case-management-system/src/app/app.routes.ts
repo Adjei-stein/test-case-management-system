cef15795f874e983a68755c7523da03a27947d66
Paul Tidings Steiner <adjeisteiner@gmail.com>
2025-01-16 20:45:14 +0000

    Fixed alignments for projects page

--- test-case-management-system/src/app/app.routes.ts
+++ test-case-management-system/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component'
 import { LoginComponent } from './pages/login/login.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path: 'dashboard',
         component: DashboardComponent
+    },
+    {
+        path: 'projects',
+        component: ProjectsComponent
     }
 ];
--- test-case-management-system/src/app/pages/projects/projects.component.ts
+++ test-case-management-system/src/app/pages/projects/projects.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
